6c87fee712f19f21db7a75ab2c406273755baa13
Eddie Hurtig <eddie@hurtigtechnologies.com>
2014-10-10 18:01:45 -0400

    The Bulk Edit System... pretty sweet

---
+++ app/assets/javascripts/bulk.js
@@ -0,0 +1,63 @@
+/**
+ * Created by ehurtig on 10/10/14.
+ */
+
+
+
+(function($){
+    $(document).ready(function () {
+    /* Selectors */
+    var bulk_select = '.bulk_select option:selected';
+    var bulk_apply  = '.bulk_apply';
+    var bulk_checks  = '.bulk_check';
+    var bulk_row = '.row'
+    var bulk_check_all = '.bulk_check_all';
+
+    $(bulk_check_all).click(function () {
+        if ($(this).is(':checked')) {
+            $(bulk_checks).prop('checked', true);
+        } else {
+            $(bulk_checks).prop('checked', false);
+        }
+    });
+
+    $(bulk_apply).click(function () {
+        var action_endpoint = $(bulk_select).data('endpoint');
+        var method          = $(bulk_select).data('method');
+
+
+        var ids = [];
+
+        $(bulk_checks + ':checked').each(function (i, checkbox) {
+            var $checkbox = $(checkbox);
+            var id = $checkbox.attr('id').split('_')[2];
+            ids.push(parseInt(id));
+        });
+
+        $.each(ids, function (i, id) {
+            $.ajax({
+                url: action_endpoint.replace(/#/g, id),
+                method: method,
+                success: function(msg) {
+                    Alerts.success('Deleted Event' + id, '', {timeout: 3000})
+                    $('#bulk_check_' + id).parents('.data-row').fadeOut()
+                },
+                error: function(msg) {
+                    console.log(msg);
+                    Alerts.error('Failed to Delete Event ' + id, 'Ajax Call to ' + action_endpoint.replaceAll('#',id) + '.  See Console for Log');
+                }
+
+            });
+        })
+
+
+
+
+    });
+
+    });
+
+
+
+})(jQuery);
+
