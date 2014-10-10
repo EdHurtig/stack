19b43956de99ae742fa7832ef49eb6b25e83be6e
Eddie Hurtig <eddie@hurtigtechnologies.com>
2014-10-10 18:00:57 -0400

    Refactored how the source_name was computed
    I'm sure there is a better way to do foreign key relationships

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  @_source_name = ""
+  @_source = ""
 
   def style_class
     if "error" == level or "critical" == level or "fail" == level
@@ -24,16 +24,21 @@ class Event < ActiveRecord::Base
 
   end
 
+  # Returns the name of the source that reported this event
+  # Returns empty string if there is no source linked to the event
   def source_name
-    if @_source_name
-      @_source_name
+    if source
+      # Lookup the Source Record by the Source ID of this event
+      @_source = Source.find(source)
+      if @_source
+        @_source = @_source.name
+      else
+        @_source = ''
+      end
     else
-      @_source_name = Source.find(source).name
+      ''
     end
 
+
   end
-
-
-
-
 end
