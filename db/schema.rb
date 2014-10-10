bc559de88763eaa6d4b6fed488ac9ca01e66b45b
Eddie Hurtig <eddie@hurtigtechnologies.com>
2014-10-10 17:51:04 -0400

    Added by column to determine how event was added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004152141) do
+ActiveRecord::Schema.define(version: 20141010143353) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141004152141) do
     t.integer  "source"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "by"
   end
 
   create_table "facts", force: true do |t|
